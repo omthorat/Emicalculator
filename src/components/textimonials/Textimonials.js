57fcf88336856229def8d3059363d0ea04f7b3b1
omthorat <114384305+omthorat@users.noreply.github.com>
2023-02-21 23:37:37 +0530

    textimonials

--- src/components/textimonials/Textimonials.js
+++ src/components/textimonials/Textimonials.js
@@ -2,7 +2,63 @@ import React from 'react'
 import "./textimonials.scss"
 function Textimonials() {
   return (
-    <div className='textimonials' id='Textimonials'>Textimonials</div>
+    <div className='textimonials' id='Textimonials'>
+      <h1>Textimonials</h1>
+      <div className="container">
+      
+        <div className="item">
+          <div className="top">
+          <img src="images/right-arrow.png" className='arrow' alt="" />
+          <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className='photo' alt="" />
+
+          <img src="images/twitter.png" className='platform' alt="" />
+          </div>
+           <div className='middle'>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde maxime iste molestiae qui vitae exercitationem cum ratione repellat. Dolor soluta iure dolores impedit repudiandae consequuntur corporis officia accusamus iusto commodi, nulla eaque sit! Eaque.</p>
+           </div>
+          
+        <div className="bottom">
+        <h3>omkar Thorat</h3>
+          <h6>senior developer</h6>
+        </div>
+          
+        </div>
+        <div className="item">
+          <div className="top">
+          <img src="images/right-arrow.png" className='arrow' alt="" />
+          <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className='photo' alt="" />
+
+          <img src="images/twitter.png" className='platform' alt="" />
+          </div>
+           <div className='middle'>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde maxime iste molestiae qui vitae exercitationem cum ratione repellat. Dolor soluta iure dolores impedit repudiandae consequuntur corporis officia accusamus iusto commodi, nulla eaque sit! Eaque.</p>
+           </div>
+          
+        <div className="bottom">
+        <h3>omkar Thorat</h3>
+          <h6>senior developer</h6>
+        </div>
+          
+        </div>
+        <div className="item">
+          <div className="top">
+          <img src="images/right-arrow.png" className='arrow' alt="" />
+          <img src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className='photo' alt="" />
+
+          <img src="images/twitter.png" className='platform' alt="" />
+          </div>
+           <div className='middle'>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde maxime iste molestiae qui vitae exercitationem cum ratione repellat. Dolor soluta iure dolores impedit repudiandae consequuntur corporis officia accusamus iusto commodi, nulla eaque sit! Eaque.</p>
+           </div>
+          
+        <div className="bottom">
+        <h3>omkar Thorat</h3>
+          <h6>senior developer</h6>
+        </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
