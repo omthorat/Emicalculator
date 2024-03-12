a56a2c564058fc2a77e9134ba0b78054b500c089
Omkar thorat <114384305+omthorat@users.noreply.github.com>
2024-03-12 08:48:33 +0000

    feat: new project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import LandingPage from "./components/LandingPage/LandingPage";
-import { BrowserRouter as Router, Route, Routes }  
-    from 'react-router-dom';
-import HomePage from "./components/HomePage/HomePage";
+// import LandingPage from "./components/LandingPage/LandingPage";
+// import { BrowserRouter as Router, Route, Routes }  
+//     from 'react-router-dom';
+// import HomePage from "./components/HomePage/HomePage";
 function App() {
   return (
-    <><Router>
+    <>
+    <h1 className="text-5xl border-l-amber-300 ">welcome</h1>
+    {/* <Router>
     <Routes>
       <Route path="/home" element={<HomePage/>}/>
       <Route path="/"
           element={<LandingPage/>}/>
           </Routes>
-    </Router>
+    </Router> */}
     </>
   );
 }
